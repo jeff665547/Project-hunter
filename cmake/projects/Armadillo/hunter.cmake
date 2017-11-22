e07def3b344f832f66c7c9edff64b2f58b6c243d
john <johnidfet@gmail.com>
2017-11-22 19:15:04 +0800

    fix sha id

--- cmake/projects/Armadillo/hunter.cmake
+++ cmake/projects/Armadillo/hunter.cmake
@@ -11,13 +11,13 @@ include(hunter_download)
 
 hunter_add_version(
     PACKAGE_NAME Armadillo
-    VERSION 7.800
+    VERSION "7.800"
     URL "https://github.com/conradsnicta/armadillo-code/archive/7.800.x.zip"
-    SHA1 1314e433b09a77455647e84375292a2b0a16ac94
+    SHA1 a8930fe230785574a71eaaf3e53c7f8aafb1a1dd 
 )
 
 
-hunter_configuration_types(Armadillo CONFIGURATION_TYPES Release)
+# hunter_configuration_types(Armadillo CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(Armadillo)
 hunter_download(
