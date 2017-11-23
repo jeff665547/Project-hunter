ccd85a836e17d5a9fe64f2c632d1be6c49788e15
john <johnidfet@gmail.com>
2017-11-23 21:51:11 +0800

    add arpack

---
+++ cmake/projects/ARPACK/hunter.cmake
@@ -0,0 +1,28 @@
+# Copyright (c) 2016, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_configuration_types)
+include(hunter_pick_scheme)
+include(hunter_download)
+
+hunter_add_version(
+    PACKAGE_NAME ARPACK
+    VERSION "3.5.0"
+    URL "https://github.com/CHChang810716/arpack-ng/archive/master.zip"
+    SHA1 "acbbe14b03b323f84236553872396345204dea1e"
+)
+
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(ARPACK)
+set( UNRELOCATABLE_TEXT_FILES
+    "arpack-ng-config.cmake"
+)
+hunter_download(
+    PACKAGE_NAME ARPACK
+    PACKAGE_UNRELOCATABLE_TEXT_FILES ${UNRELOCATABLE_TEXT_FILES} 
+)
