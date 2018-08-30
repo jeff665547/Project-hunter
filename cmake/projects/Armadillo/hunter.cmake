98d6d74ed55c70dd3af7f37007221c6916ae3068
ChiaHuaChang <johnidfet@gmail.com>
2018-08-30 13:26:21 +0800

    armadillo worable

--- cmake/projects/Armadillo/hunter.cmake
+++ cmake/projects/Armadillo/hunter.cmake
@@ -10,23 +10,16 @@ include(hunter_pick_scheme)
 include(hunter_download)
 
 hunter_add_version(
-    PACKAGE_NAME Armadillo
-    VERSION "7.800"
-    URL "https://github.com/CHChang810716/armadillo-code/archive/5d7610e34ad2647a87f3bc0ac23fff6d89611147.zip"
-    SHA1 "efdf49083de6e7eb2a70fde06482f61614a5163d"
+    PACKAGE_NAME armadillo
+    VERSION "8.200.x-p0"
+    URL "https://github.com/CHChang810716/armadillo-code/archive/27363e2c65b3aa4ac3dd516e87e391bf7a46b5ac.zip"
+    SHA1 "65ae890d68f7f88a0d0d26271c71d7020eafa684"
 )
 
 
 # hunter_configuration_types(Armadillo CONFIGURATION_TYPES Release)
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(Armadillo)
-set( UNRELOCATABLE_TEXT_FILES
-    share/Armadillo/CMake/ArmadilloConfig.cmake
-    share/Armadillo/CMake/ArmadilloLibraryDepends-debug.cmake
-    share/Armadillo/CMake/ArmadilloLibraryDepends-release.cmake
-    share/Armadillo/CMake/ArmadilloLibraryDepends.cmake
-)
+hunter_cacheable(armadillo)
 hunter_download(
-    PACKAGE_NAME Armadillo
-    PACKAGE_UNRELOCATABLE_TEXT_FILES ${UNRELOCATABLE_TEXT_FILES} 
+    PACKAGE_NAME armadillo
 )
