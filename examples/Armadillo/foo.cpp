2fa8f511d1f158800954ca2f44fff19661cef720
Chia-Hua Chang <CHChang810716@gmail.com>
2018-08-30 05:37:35 +0000

    remove unused file

--- examples/Armadillo/foo.cpp
+++
@@ -1,8 +0,0 @@
-#include <armadillo>
-#include <iostream>
-int main() 
-{
-    arma::mat A(5, 5, arma::fill::randu);
-    arma::mat B(5, 5, arma::fill::randn);
-    std::cout << A * B << std::endl;
-}
