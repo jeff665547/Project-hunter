5f09e71dee8703ea9d7de10198e1d31ef55eb771
ChiaHuaChang <johnidfet@gmail.com>
2018-09-07 19:39:04 +0800

    update libsimdpp

--- cmake/projects/libsimdpp/hunter.cmake
+++ cmake/projects/libsimdpp/hunter.cmake
@@ -17,6 +17,16 @@ hunter_add_version(
     SHA1
     0bf0a759fb4d41104e5d96bf85be8c2ef851d284
 )
+hunter_add_version(
+    PACKAGE_NAME
+    libsimdpp
+    VERSION
+    2.x-p0
+    URL
+    "https://github.com/CHChang810716/libsimdpp/archive/c81f999e51edd9fce76ab9c0e858cf64c2a14034.zip"
+    SHA1
+    ad22abd3e376ab38ab022af8695232df2c4b02fd
+)
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(libsimdpp)
--- examples/libsimdpp/main.cpp
+++ examples/libsimdpp/main.cpp
@@ -1,4 +1,6 @@
 #include <simdpp/simd.h>
 int main() {
-
+    simdpp::uint8<32> a;
+    simdpp::uint8<32> b;
+    auto c = a + b;
 }
