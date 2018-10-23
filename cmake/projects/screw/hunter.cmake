a4e45e1c0efae9710825b5d8ad40a0d213a19154
ChiaHuaChang <johnidfet@gmail.com>
2018-10-23 18:21:51 +0800

    add screw

---
+++ cmake/projects/screw/hunter.cmake
@@ -0,0 +1,24 @@
+# Copyright (c) 2013-2018, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    screw
+    VERSION
+    "0.0.1"
+    URL
+    "http://gitlab.centrilliontech.com.tw:10080/centrillion/screw/repository/0.0.1/archive.tar.gz"
+    SHA1
+    167a98d7b78fc30af5aecad16993685397298ec5
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(screw)
+hunter_download(PACKAGE_NAME screw)
