4220f44826e079dc509365cedfde4617ef21ef1d
C.H Chang <CHChang810716@gmail.com>
2018-11-06 04:03:26 +0000

    update arpack

--- cmake/projects/ARPACK-NG/hunter.cmake
+++ cmake/projects/ARPACK-NG/hunter.cmake
@@ -12,9 +12,15 @@ include(hunter_cmake_args)
 
 hunter_add_version(
     PACKAGE_NAME ARPACK-NG
-    VERSION "3.6.0"
+    VERSION "3.5.0-p0"
     URL "https://github.com/CHChang810716/arpack-ng/archive/3b5b2386f8eddb8b77d20d0dd5d284e374c230a5.zip"
     SHA1 "24db79ce050156c07513d3c01e9d04d551eb201d"
+)
+hunter_add_version(
+    PACKAGE_NAME ARPACK-NG
+    VERSION "3.5.0-p1"
+    URL "https://github.com/CHChang810716/arpack-ng/archive/c0e824148a572476088e20f79a748011d8b0e923.zip"
+    SHA1 "3f929a15a733066083fadcd5dee06da9ccf91e4e"
 )
 
 
