849a8e2192c17d0d8ae54a7a9cc6dca9d8850298
C.H Chang <CHChang810716@gmail.com>
2018-11-06 04:58:25 +0000

    update armadillo

--- cmake/projects/armadillo/hunter.cmake
+++ cmake/projects/armadillo/hunter.cmake
@@ -15,6 +15,12 @@ hunter_add_version(
     URL "https://github.com/CHChang810716/armadillo-code/archive/27363e2c65b3aa4ac3dd516e87e391bf7a46b5ac.zip"
     SHA1 "65ae890d68f7f88a0d0d26271c71d7020eafa684"
 )
+hunter_add_version(
+    PACKAGE_NAME armadillo
+    VERSION "8.200.x-p1"
+    URL "https://github.com/CHChang810716/armadillo-code/archive/8.200.x-p1.zip"
+    SHA1 "a87e2e0924823101ce2fb96ea5bd8e8102d02590"
+)
 
 
 # hunter_configuration_types(Armadillo CONFIGURATION_TYPES Release)
