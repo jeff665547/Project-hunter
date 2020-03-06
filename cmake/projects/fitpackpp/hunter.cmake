51edbee0529ca281dcfa7befbc13c052c9404af1
ChiaHuaChang <johnidfet@gmail.com>
2020-03-06 14:43:46 +0800

    update fitpackpp

--- cmake/projects/fitpackpp/hunter.cmake
+++ cmake/projects/fitpackpp/hunter.cmake
@@ -19,6 +19,16 @@ hunter_add_version(
     SHA1
     bbf3186ef9e73ba9bbe01ddcba1617bc767f8a20
 )
+hunter_add_version(
+    PACKAGE_NAME
+    fitpackpp
+    VERSION
+    "1.0.0-p2"
+    URL
+    "https://github.com/CHChang810716/fitpackpp/archive/1.0.0-p2.zip"
+    SHA1
+    de1593c5f62aab6abedd70f4e26e7237ff5551de
+)
 
 # Pick a download scheme
 hunter_pick_scheme(DEFAULT url_sha1_cmake) # use scheme for cmake projects
