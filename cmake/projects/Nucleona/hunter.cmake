0eff4e10b7e4e0008537761505fcb6b1d5c6460b
noreason <ken5112840801@gmail.com>
2021-04-06 17:13:57 +0800

    Update Nucleona

--- cmake/projects/Nucleona/hunter.cmake
+++ cmake/projects/Nucleona/hunter.cmake
@@ -127,6 +127,12 @@ hunter_add_version(
 )
 hunter_add_version(
     PACKAGE_NAME Nucleona
+    VERSION "0.5.8"
+    URL "http://gitlab.centrilliontech.com.tw:10080/centrillion/Nucleona/repository/v0.5.7/archive.tar.gz?private_token=Mb9qZ6zbD4WivhkSzAPC"
+    SHA1 d68c33f01bbd82ae7f6f462254805110127ada39
+)
+hunter_add_version(
+    PACKAGE_NAME Nucleona
     VERSION "0.6.1"
     URL "http://gitlab.centrilliontech.com.tw:10080/centrillion/Nucleona/repository/v0.6.1/archive.tar.gz?private_token=Mb9qZ6zbD4WivhkSzAPC"
     SHA1 7aedb0d3109564d4e4d944e91a303cff1aa50570
