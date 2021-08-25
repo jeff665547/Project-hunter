0bec3c77aedc38b02cb78b10ae47977103ca5949
joyeli <joyeli@outlook.com>
2021-08-25 13:41:07 +0800

    add libcbor in projects

---
+++ cmake/projects/libcbor/hunter.cmake
@@ -0,0 +1,28 @@
+# Copyright (c) 2015, Steve Brain
+# Copyright (c) 2017, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+include(hunter_protected_sources)
+
+hunter_add_version(
+    PACKAGE_NAME libcbor
+    VERSION "0.8.0"
+    URL "http://gitlab.centrilliontech.com.tw:10080/centrillion/libcbor/repository/v0.8.0/archive.tar.gz?private_token=Mb9qZ6zbD4WivhkSzAPC"
+    SHA1 e9c4582a3843248065384ca44e315c45af755fa1
+)
+hunter_cmake_args(
+    libcbor
+    CMAKE_ARGS
+        BUILD_TESTS=OFF
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(libcbor)
+hunter_download(PACKAGE_NAME libcbor)
