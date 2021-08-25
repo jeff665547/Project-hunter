1c5e49f2fb1ea814f83e9cef189a70f2fdf83a08
joyeli <joyeli@outlook.com>
2021-08-25 16:32:58 +0800

    fix libfido2

--- cmake/projects/libfido2/hunter.cmake
+++ cmake/projects/libfido2/hunter.cmake
@@ -15,7 +15,7 @@ hunter_add_version(
     PACKAGE_NAME libfido2
     VERSION "1.8.0"
     URL "http://gitlab.centrilliontech.com.tw:10088/api/v4/projects/265/repository/archive?sha=091cebe448f366c5efdbcd796d1395d175470552&private_token=Mb9qZ6zbD4WivhkSzAPC"
-    SHA1 0d0816a7302843fd62911470206cdf527e0218c7
+    SHA1 5f895841c919f3e67f7601d4c65e950dd0efbe27
 )
 hunter_cmake_args(
     libfido2
