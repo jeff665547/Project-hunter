07e4b68f5bc3c91f55a7c1172b6f4b5a78a7e0b1
joyeli <joyeli@outlook.com>
2021-08-25 23:48:38 +0800

    test, change source from gitlab to github

--- cmake/projects/libfido2/hunter.cmake
+++ cmake/projects/libfido2/hunter.cmake
@@ -14,7 +14,7 @@ include(hunter_protected_sources)
 hunter_add_version(
     PACKAGE_NAME libfido2
     VERSION "1.8.0"
-    URL "http://gitlab.centrilliontech.com.tw:10088/api/v4/projects/265/repository/archive?sha=091cebe448f366c5efdbcd796d1395d175470552&private_token=Mb9qZ6zbD4WivhkSzAPC"
+    URL "https://github.com/Yubico/libfido2/archive/refs/tags/1.8.0.tar.gz"
     SHA1 5f895841c919f3e67f7601d4c65e950dd0efbe27
 )
 hunter_cmake_args(
